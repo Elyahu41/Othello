27af5bdb9edbc2a17aa7f402eb313d0d0a782871
Ely Jacobi <47622014+Elyahu41@users.noreply.github.com>
2020-02-17 13:51:06 -0500

    Again

--- src/Othello/Main.java
+++ src/Othello/Main.java
@@ -1,6 +1,6 @@
 package Othello;
 public class Main {
-// Elyahu Jacobi
+//Elyahu Jacobi
     public static void main(String[] args) {
         OthelloModelInterface othelloModel = new OthelloModel();
         new OthelloWindow(othelloModel);
